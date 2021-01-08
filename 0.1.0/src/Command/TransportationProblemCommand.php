9bfc1c309d784ecf294c4549f17108e7bbd13b2f
sergeiki <sergei.ksmith@gmail.com>
2021-01-09 00:35:10 +0200

    , in manifest.json

--- 0.1.0/src/Command/TransportationProblemCommand.php
+++ 0.1.0/src/Command/TransportationProblemCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use sergeiki\trpr\TransportationProblem;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableSeparator;
@@ -10,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use sergeiki\trpr;
+
 
 class TransportationProblemCommand extends Command
 {
